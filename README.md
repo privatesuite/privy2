a73d6992267027dc601e247e4b76ffc5dd6bdd5e
SuperAuguste <auguste.rame@gmail.com>
2019-07-17 08:05:07 +0200

    Initial Commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# privy2
+
+Version 2 of Private Suite's website is here!
+
+FULL PAGE | https://cdn.discordapp.com/attachments/524021230559428608/598341923983458317/psm_website_comp_v1-FULL.png
+DROPDOWNS | https://cdn.discordapp.com/attachments/524021230559428608/598342037598765091/psm_website_comp_v1-dropdowns.png
+POST | https://cdn.discordapp.com/attachments/524021230559428608/600546806027255841/psm_website_comp_v1-articlesB.png
---
+++ js/app.js
@@ -0,0 +1,156 @@
+const API_ROOT = `https://${location.hostname}/api`;
+
+const routes = {
+
+	"/": {
+
+		view: "index.ejs"
+
+	},
+
+	"/about": {
+
+		view: "about.ejs"
+
+	},
+
+	"/privacy": {
+
+		view: "privacy.ejs"
+
+	},
+
+	"/post/*": {
+
+		view: "post.ejs"
+
+	}
+
+}
+
+const cache = {}
+
+// View
+
+function slugify (slug) {
+
+	return encodeURIComponent(slug.replace(/ /g, "_").toLowerCase());
+
+}
+
+function deslugify (slug) {
+
+	return decodeURIComponent(slug.replace(/_/g, " "));
+
+}
+
+async function render (view, data) {
+
+	data = {
+
+		...data,
+		render,
+
+		currentRoute,
+		
+		capitalize (text) {
+
+			return text[0].toUpperCase() + text.slice(1);
+
+		},
+
+		async fetchElement (title) {
+
+			var json = await (await fetch(`${API_ROOT}/elements`)).json();
+			var element = json.find(_ => _.fields.title.toLowerCase() === title);
+
+			return element.fields;
+
+		},
+
+		async fetchPost (slug) {
+
+			var json = await (await fetch(`${API_ROOT}/elements`)).json();
+			var element = json.find(_ => _.fields.title.toLowerCase() === deslugify(slug));
+
+			return element.fields;
+
+		},
+
+		file (path) {
+
+			return `${API_ROOT}/file/${path}`;
+
+		}
+
+	}
+
+	var view_data = cache[view] || await (await fetch(`/views/${view}`)).text();
+
+	cache[view] = view_data;
+
+	return ejs.render(view_data, data, {
+
+		async: true
+
+	});
+
+}
+
+// Router
+
+function currentRoute () {
+
+	return location.hash.replace("#", "") ? location.hash.replace("#", "") : "/";
+
+}
+
+// All together now
+
+function currentRouteData () {
+
+	// Checks if currentRoute is equal to any routes
+	// Also checks if a route ends with * and that the route it is compared too matches the beginning of the route (eg. /post/* and /post/potatoes_are_tasty)
+	for (const route of Object.keys(routes)) if (route === currentRoute() || (route.endsWith("*") && currentRoute().startsWith(route.slice(0, route.length - 1)))) return routes[route];
+
+}
+
+async function load () {
+
+	if (currentRouteData()) {
+
+		document.body.innerHTML = await render(currentRouteData().view);
+
+	} else {
+
+		location.hash = "#/";
+
+	}
+
+}
+
+function main () {
+
+	load();
+
+	document.addEventListener("click", event => {
+
+		if (event.target.parentElement && event.target.parentElement.classList.contains("dots")) {
+
+			const index = [...event.target.parentElement.children].indexOf(event.target);
+
+			event.target.parentElement.querySelector(".selected").classList.remove("selected");
+			event.target.classList.add("selected");
+
+			event.target.parentElement.parentElement.querySelector(".current_slide").classList.remove("current_slide");
+			event.target.parentElement.parentElement.children[index].classList.add("current_slide");
+
+		}
+
+	});
+
+	window.addEventListener("hashchange", load);
+
+}
+
+main();
